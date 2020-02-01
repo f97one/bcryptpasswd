ded3341658966c0685a3c951a1c74877ccc67494
HAJIME Fukuna <f97one@gmail.com>
2020-02-01 04:10:21 +0000

    72文字チェックを入れてみた。

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
 	flag.Parse()
+
+	pw := flag.Arg(0)
+	if len(pw) > 72 {
+		fmt.Println("WARNING : The string passed as an argument is longer than 72 characters.")
+		fmt.Println("          Strings after the 73rd character are ignored.")
+	}
+
 	hash, err := bcrypt.GenerateFromPassword([]byte(flag.Arg(0)), bcrypt.DefaultCost)
 	if err != nil {
 		return
